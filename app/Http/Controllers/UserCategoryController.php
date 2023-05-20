0062edc1903ade32a8a20612fb75424c19a7ea90
Mohammad Fathi <fathimohammad1999@gmail.com>
2023-05-20 10:57:16 +0330

    Category CRUD controller developed

--- app/Http/Controllers/UserCategoryController.php
+++ app/Http/Controllers/UserCategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\CategoryCollection;
+use App\Http\Resources\CategoryResource;
 use App\Http\Responses\APIResponse;
 use App\Models\Category;
 use Illuminate\Http\Request;
@@ -10,21 +11,23 @@ use Illuminate\Support\Facades\Auth;
 
 class UserCategoryController extends Controller
 {
-    public $limit;
-    public function __construct(
-        Request $request
-    )
-    {
-        $this->limit = ( $request->limit && $request->limit >= 1) ? $request->limit : 10;
-    }
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        return CategoryCollection::collection(Category::paginate($this->limit));
-        // $categories = CategoryCollection::collection(Category::all())->paginate(10);
-        // return APIResponse::json($data= $categories);
+        $request->validate([
+            'limit' => ['numeric', 'min:1'],
+            'search' => ['string'],
+        ]);
+
+        $limit = $request->limit ? $request->limit : 10;
+        $category = Category::where('context','LIKE',"%$request->search%")
+                    ->paginate($limit);
+
+        $category_collection =  CategoryCollection::collection($category);
+
+        return APIResponse::json($data = $category_collection);
     }
 
     /**
@@ -32,19 +35,22 @@ class UserCategoryController extends Controller
      */
     public function store(Request $request)
     {
-        $request ->validate([
-            'title' => ['required','string','unique:categories'],
-            'description' => ['string']
+        $request->validate([
+            'title' => ['required','max:120','unique:categories,title'],
+            'description' => ['string'],
         ]);
 
         $user = Auth::user();
 
-        $new_category = $user->categories()->create([
+        $new_category =$user->categories()->create([
             'title' => $request->title,
-            'description' => $request->description
+            'description' => $request->description,
         ]);
-        // return $new_category;
-        return APIResponse::json('category created',false, new UserCategoryResource($new_category));
+
+        $new_category_resource = new CategoryResource($new_category);
+
+
+        return APIResponse::json('post created',$new_category_resource);
     }
 
     /**
@@ -52,7 +58,9 @@ class UserCategoryController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $category = Category::findOrFail($id);
+        $category_resource = new CategoryResource($category);
+        return APIResponse::json($data=$category_resource);
     }
 
     /**
@@ -60,7 +68,22 @@ class UserCategoryController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate([
+            'title' => ['required','max:120','unique:categories,title'],
+            'description' => ['string'],
+        ]);
+
+        $category = Category::findOrFail($id);
+
+        $updated_category =$category->update([
+            'title' => $request->title,
+            'description' => $request->description,
+        ]);
+
+        $updated_post_resorce = new CategoryResource($updated_category);
+
+        return APIResponse::json('The post updated susccessfuly');
+
     }
 
     /**
@@ -68,6 +91,10 @@ class UserCategoryController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+
+        $category = Category::findOrFail($id);
+        $category->delete();
+        return APIResponse::json($message = 'The post deleted successfuly',$data=$category,$status=204);
+
     }
 }
